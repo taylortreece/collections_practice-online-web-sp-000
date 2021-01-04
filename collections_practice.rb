ce713fcbd602dc51233dd2bcb57991cedd00c491
Taylor Treece <tayloratreece@gmail.com>
2021-01-04 20:09:36 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort { |x, y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.collect do |n|
+  n[2] = "$"
+  new_array << n
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select do |n|
+    n.chr == "a"
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, num|
+    sum + num
+  end
+end
+
+
+def add_s(array)
+  array.collect.with_index do |word, index|
+    if index == 1
+      word
+    else
+      word << "s"
+    end
+  end
+end
